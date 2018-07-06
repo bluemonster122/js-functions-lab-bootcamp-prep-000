435fe0232a225b9cd07f1c9fe1f00f60949ddfad
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 13:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(name, days) {
-  return "It's ${days} days until ${holiday}!"
+  return "It's ${days} days until ${name}!"
 }
